2afc4f393d639556126ebd10895e111bb7625f5b
Jack Yang <jackyangzzh@gmail.com>
2022-06-25 22:10:26 -0700

    Completed first window

--- Vulkan-Playground/Vulkan-Playground/FirstApp.h
+++ Vulkan-Playground/Vulkan-Playground/FirstApp.h
@@ -1,4 +1,15 @@
 #pragma once
 #include "lve_window.h"
 
+namespace lve {
+	class FirstApp {
+	public:
+		static constexpr int WIDTH = 800;
+		static constexpr int HEIGHT = 800;
 
+		void run();
+
+	private:
+		LveWindow lveWindow{ WIDTH, HEIGHT, "Hello Vulkan!" };
+	};
+}
--- Vulkan-Playground/Vulkan-Playground/lve_window.h
+++ Vulkan-Playground/Vulkan-Playground/lve_window.h
@@ -12,6 +12,11 @@ namespace lve {
 		LveWindow(int w, int h, string name);
 		~LveWindow();
 
+		LveWindow(const LveWindow&) = delete;
+		LveWindow& operator= (const LveWindow&) = delete;
+
+		bool shouldClose() { return glfwWindowShouldClose(window); }
+
 	private:
 		void initWindow();
 
