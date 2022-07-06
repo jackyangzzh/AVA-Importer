739af4630bb76cbd85f74156865a2a829590158b
Jack Yang <jackyangzzh@gmail.com>
2022-07-05 21:14:30 -0700

    Added lve device code

--- Vulkan-Playground/Vulkan-Playground/lve_window.cpp
+++ Vulkan-Playground/Vulkan-Playground/lve_window.cpp
@@ -1,4 +1,7 @@
 #include "lve_window.h"
+#include <iostream>
+
+using namespace std;
 
 namespace lve {
 	LveWindow::LveWindow(int w, int h, string name) : width{ w }, height{ h }, windowName{ name } {
@@ -8,6 +11,13 @@ namespace lve {
 	LveWindow::~LveWindow() {
 		glfwDestroyWindow(window);
 		glfwTerminate();
+	}
+
+	void LveWindow::createWindowSurface(VkInstance instance, VkSurfaceKHR* surface)
+	{
+		if (glfwCreateWindowSurface(instance, window, nullptr, surface) != VK_SUCCESS) {
+			throw runtime_error("failed to create window surface");
+		}
 	}
 
 	void LveWindow::initWindow() {
