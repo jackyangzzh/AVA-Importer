05a4f4e55a01140f4f11755651f3550b13662970
Jack Yang <jackyangzzh@gmail.com>
2022-07-18 22:09:03 -0700

    Save progress on create window

--- Source Files/main.cpp
+++ Source Files/main.cpp
@@ -1,31 +1,25 @@
-#define GLFW_INCLUDE_VULKAN
+#define GLFW_INCLINE_VULKAN
 #include <GLFW/glfw3.h>
 
-#define GLM_FORCE_RADIANS
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#include <glm/glm.hpp>
-#include <glm/mat4x4.hpp>
-
 #include <iostream>
+#include <stdexcept>
+#include <vector>
+
+using namespace std;
+
+GLFWwindow* window;
+
+void initWindow(string wName = "Test Window", const int width = 800, const int height = 600) {
+	glfwInit();
+
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+	
+	window = glfwCreateWindow(width, height, wName.c_str(), nullptr, nullptr);
+}
 
 int main() {
-	glfwInit();
-	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-
-	GLFWwindow* window = glfwCreateWindow(800, 600, "Test Window", nullptr, nullptr);
-
-	uint32_t extensionCount = 0;
-	vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
-
-	printf("Extension count: %i\n", extensionCount);
-
-	while (!glfwWindowShouldClose(window)) {
-		glfwPollEvents();
-	}
-
-	glfwDestroyWindow(window);
-
-	glfwTerminate();
+	initWindow("Test Window", 800, 600);
 
 	return 0;
 }
---
+++ VulkanRenderer.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#define GLFW_INCLINE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <stdexcept>
+#include <vector>
+
+class VulkanRenderer
+{
+	VulkanRenderer();
+	~VulkanRenderer();
+};
+
